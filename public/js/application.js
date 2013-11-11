5bbdaac38e5327dc49284a16bfe77a291ca8ae1c
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-11 04:14:42 -0600

    Styled home / login page

--- public/js/application.js
+++ public/js/application.js
@@ -7,6 +7,32 @@ $(document).ready(function() {
   		$(".container").append(form);
   	});
   });
+
+
+
+
+
+
+
+
+
+
+  // Front end eye-candy stuff
+
+
+  // Show login form on click
+  $("#login_logout").click(function() {
+    console.log("clicked");
+    $(".homecontent").animate({height:"100%"});
+    $(".loginform").slideDown(1000);
+  });
+  $("#loginbutton").click(function() {
+    console.log("clicked2");
+    $(".homecontent").animate({height:"0%"});
+    $(".loginform").slideUp(1000);
+
+  });
+
 });
 
 
